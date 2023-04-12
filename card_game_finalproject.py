8452462f1998c9783337cfc53ce106ce8a4bb675
Hanan Debencho <hdebench@terpmail.umd.edu>
2023-04-12 17:33:33 -0400

    started code outline

---
+++ card_game_finalproject.py
@@ -0,0 +1,18 @@
+class Player:
+    """Both computer and human players
+    Attributes: 
+        name(str) - name of player
+        home(int) - num of points player has
+    """
+class HumanPlayer(Player):
+    """human player playing game
+
+    Args:
+        Player (class): inherits everything the player class has
+    """
+class ComputerPlayer(Player):
+    """computer player playing game
+
+    Args:
+        Player (class): inherits everything the player class has
+    """
