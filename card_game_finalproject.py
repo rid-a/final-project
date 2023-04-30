c11ca08f4ea3071e86b353e1f35ff52c1481e802
Rida Zurga <rzurga@terpmail.umd.edu>
2023-04-30 12:34:01 -0400

    Updated code skeleton

--- card_game_finalproject.py
+++ card_game_finalproject.py
@@ -1,61 +1,61 @@
 import random
+from argparse import ArgumentParser
+import sys
+
+def deck(filePath):
+    deck_list = []
+    with open(filePath, "r", encoding = "utf-8") as file:
+        # INSERT regex w/capturing groups for the number/letter + the name
+        # or maybe not here since we're not doing dict anymore
+        for line in file:
+            # create a list with '3 spades' 'A ace', etc.
+            # don't forget to use .strip() or .rstrip()
+            # name the list deck_list and delete this line of code:
+            pass
+    return deck_list
+
+
 class Player:
-    """Both computer and human players
-    Attributes: 
-        name(str) - name of player
-        home(int) - num of points player has
-        score(int)- the score of the player
-    """
     def __init__(self, name):
         self.name = name
-        self.home = []
-        self.score = 0
-        
-    def choose_card(filepath):
-        with open(filepath, encoding="utf-8") as f:
-            for line in f:
-                my_card = random.choice(line.strip())
-                return my_card
-            
-    def update_home(self, points):
-        '''
-        Updates the score of the player by adding he given points
-        
+        self.home = list()
+        self.card_value = ""
+        self.card_suit = ""
+
+    def choose_card(self, table):
+        """Represents the player choosing a card from the outer circle
         Args:
-         - points(int): The number of points that should be added to
-           the score borad
-        '''
-        score += points
+            table (Table): represents the current state of the table/game
+        Side Effects:
+            Updates the card_value and card_suit attributes with the card that was pulled.
+            Prints table.
+            Prints message to user about what card they pulled.
+        Returns:
+            random_card (str): the card the user pulled
+        """
+
+class Table:
+    """Represents the current state of the game, AKA what's on the table
     
-        
-class HumanPlayer(Player):
-    """human player playing game
-
-    Args:
-        Player (class): inherits everything the player class has
-    """
-class ComputerPlayer(Player):
-    """computer player playing game
-
-    Args:
-        Player (class): inherits everything the player class has
+    Attributes:
+        pulled_cards (list of str): cards that were already pulled
+        placed_cards (list of str): cards that were added to the middle
+        outer_cards (list of str): cards remaining in the outer circle
+        middle_cards (list of str):
+        homes (dict of str: int):
     """
 
-class Table():
-    """Information on what's on the table 
-    Attributes: 
-        remaining_cards(list of str)- cards still left upside down on table
-        middle- cards facing up 
-
-    """
-class Game():
-    """ Staging game play 
-    Attributes: 
-        players(list)- list of player instances 
-        home(int)- score of each player
-
-    """
-def main(): 
-    """Initializing the game 
-    """
+class Game:
+    """A game of Injera be Wet"""
     
+def main(cardsList, playersList):
+    """Set up and play a game of Injera be Wet"""
+    
+    
+def parse_args(argList):
+    """Parse command-line arguments"""
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    # call the main function w/all required arguments, but with 'args.' preceding it
+    # basically ur calling the main function with the command line arguments
