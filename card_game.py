01d7daa7c845739bf89ad513a63ed0ab3bc4cb85
Alisha Mahajan <alisha25@terpmail.umd.edu>
2023-05-08 11:17:36 -0400

    added deck function

--- card_game.py
+++ card_game.py
@@ -2,3 +2,11 @@ import random
 from argparse import ArgumentParser
 import sys
 import re
+
+def deck(filePath):
+    deck_list = []
+    with open(filePath, "r", encoding = "utf-8") as file:
+        deck_list = []
+        for line in file: 
+            deck_list.append(line.strip())
+    return deck_list
