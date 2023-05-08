a6eb0c3514bf3de927b5f4aed31d804680c1df88
Yasmin Said <ysaid@terpmail.umd.edu>
2023-05-08 12:24:32 -0400

    fixed indentation

--- card_game.py
+++ card_game.py
@@ -93,8 +93,8 @@ class Game:
                     "(Format like this: '3 diamonds' or 'A spades')" + 
                     "choose wisely, you only get one chance. )").strip())
                 
-            if card.card_value_suit()[0] == possible_match.card_value_suit()[0] and possible_match.card_str in table.middle_cards:
-                print ("Match successful! Both cards will be added to your home.\n")
-                self.homes[player.name] += 2
-                self.middle_cards.remove(possible_match.card_str)
-                return 
+                if card.card_value_suit()[0] == possible_match.card_value_suit()[0] and possible_match.card_str in table.middle_cards:
+                    print ("Match successful! Both cards will be added to your home.\n")
+                    self.homes[player.name] += 2
+                    self.middle_cards.remove(possible_match.card_str)
+                    return 
